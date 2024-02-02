30578c5d50f7167b2255289d6aeeaf734b731f60
macinsight <hergemoeller@b1-systems.de>
2024-02-02 15:10:52 +0100

    update
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

---
+++ assets/php/navbar.php
@@ -0,0 +1,27 @@
+	<div class="container sticky-top" id="navbar">
+		<nav class="navbar navbar-expand-lg bg-body-tertiary rounded" id="navigator">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Silentium Security</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbartoggler"
+					aria-controls="navbartoggler" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbartoggler">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<a href="#intro" class="nav-link nav-item">Home</a>
+						<a href="#images" class="nav-item nav-link px-2">Gallery</a>
+						<a href="#about" class="nav-item nav-link px-2">About</a>
+						<a href="#upcoming-operations" class="nav-item nav-link px-2">Upcoming Ops</a>
+						<a href="#units" class="nav-item nav-link px-2">Units</a>
+						<a href="#values" class="nav-item nav-link px-2">Values</a>
+					</ul>
+					<div class="d-lg-flex col-lg-3 justify-content-lg-end">
+						<a class="icon-link" href="https://discord.gg/D3U3e9seZ7">
+							<button type="submit" class="btn btn-primary"><span class="bi-discord width=" 48" height="48""></span></button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+
