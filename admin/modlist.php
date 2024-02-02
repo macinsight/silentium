d25733f9f6020009d3a6bc75cd72ff72ebade063
macinsight <hergemoeller@b1-systems.de>
2024-02-02 17:20:13 +0100

    Fix typo on modlist
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

--- admin/modlist.php
+++ admin/modlist.php
@@ -6,7 +6,7 @@ require_once 'functions.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$db_host = $_ENV['DB_HOST'T;
+$db_host = $_ENV['DB_HOST'];
 $db_user = $_ENV['DB_USER'];
 $db_password = $_ENV['DB_PASSWORD'];
 $db_database = $_ENV['DB_DATABASE'];
