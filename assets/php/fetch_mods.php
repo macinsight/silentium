02578f743187a36945318a2d334783f2c7805df9
macinsight <hergemoeller@b1-systems.de>
2024-02-02 18:28:54 +0100

    Update fetch for new table data
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -19,40 +19,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Function to fetch API response and store it in cache
-function fetchAPIResponse($apiUrl, $postData, $cacheFile)
-{
-    $context = stream_context_create([
-        'http' => [
-            'method' => 'POST',
-            'header' => 'Content-Type: application/x-www-form-urlencoded',
-            'content' => $postData
-        ]
-    ]);
 
-    $response = file_get_contents($apiUrl, false, $context);
-
-    // Save the response to cache file
-    file_put_contents($cacheFile, $response);
-
-    return $response;
-}
-
-// Function to retrieve API response from cache if available and not expired
-function retrieveAPIResponseFromCache($cacheFile, $cacheDuration)
-{
-    if (file_exists($cacheFile)) {
-        $fileModifiedTime = filemtime($cacheFile);
-        $currentTime = time();
-        $elapsedTime = $currentTime - $fileModifiedTime;
-
-        if ($elapsedTime <= $cacheDuration) {
-            return file_get_contents($cacheFile);
-        }
-    }
-
-    return false;
-}
 
 // Query to fetch mod data
 $sql = "SELECT * FROM modlist ORDER BY id ASC";
@@ -71,63 +38,18 @@ if ($result->num_rows > 0) {
     echo '<tbody>';
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
-        $modID = $row['mod_id'];
-
-        // Define cache file path
-        $cacheFile = "cache/$modID.cache";
-
-        // Check if API response is available in cache and not expired
-        $response = retrieveAPIResponseFromCache($cacheFile, $cacheDuration);
-
-        if (!$response) {
-            // Query Steam API for mod details
-            $apiUrl = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/";
-            $postData = http_build_query([
-                'itemcount' => 1,
-                'format' => 'json',
-                'publishedfileids[0]' => $modID
-            ]);
-
-            // Fetch API response and store it in cache
-            $response = fetchAPIResponse($apiUrl, $postData, $cacheFile);
-        }
-
-        // Process API response
-        $data = json_decode($response, true);
-
-        // Check if the response contains mod details
-        if ($data['response']['result'] == 1 && isset($data['response']['publishedfiledetails'][0])) {
-            $fileDetails = $data['response']['publishedfiledetails'][0];
-            $modTitle = $fileDetails['title'];
-            $fileSizeBytes = isset($fileDetails['file_size']) ? $fileDetails['file_size'] : 0;
-            $fileSizeMB = round($fileSizeBytes / (1024 * 1024), 2);
-
-            // Increment the total file size
-            $totalFileSize += $fileSizeMB;
-
-            // Output the link with the mod title
-            echo "<td><a href='https://steamcommunity.com/sharedfiles/filedetails/?id=$modID' class='link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' target='_blank'>$modTitle</a></td>";
-            echo "<td>$fileSizeMB MB</td>";
-        } else {
-            // Output "N/A" if mod details are not available
-            echo "<td>N/A</td>";
-            echo "<td>N/A</td>";
-        }
-
+        $link = $row['link'];
         echo "<td>";
-        if ($row['mod_required'] == 1) {
-            echo '<span class="badge rounded-pill text-bg-info">Required</span>';
-        } else {
-            echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
-        }
+        $description = $row['description'];
+        echo "</td>";
+        echo "<td>";
+        $image = $row['image'];
         echo "</td>";
         echo "</tr>";
     }
     echo '</tbody>';
     echo '</table>';
 
-    // Display the total file size
-    echo "<p>Total File Size: $totalFileSize MB</p>";
 } else {
     echo "<p>No Mods found.</p>";
 }
