bd0e3db484dac809db11517cdaf270d7803348cf
macinsight <hergemoeller@b1-systems.de>
2024-02-02 18:48:22 +0100

    Update fetchmods
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -30,14 +30,14 @@ $totalFileSize = 0;
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
     echo '<table class="table table-hover">';
-    echo '<thead><tr><th>Mod Name</th>th>Image</th></tr></thead>';
+    echo '<thead><tr><th>Mod Name</th><th>Image</th></tr></thead>';
     echo '<tbody>';
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         $link = $row['link'];
         $description = $row['description'];
         $image = $row['image'];
-        echo "<td><a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href='$link'>$description</a></td>";
+        echo "<td><a class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='$link'>$description</a></td>";
         echo "<td><img src='$image' alt='$description' style='max-width: 100px;'></td>";
         echo "</tr>";
     }
