66bd176ded661a7201a6d4da2e2d5d02a5cc399d
macinsight <hergemoeller@b1-systems.de>
2024-02-02 18:38:40 +0100

    Update fetchmods
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -25,26 +25,21 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM modlist ORDER BY id ASC";
 $result = $conn->query($sql);
 
-// Cache duration in seconds (1 day)
-$cacheDuration = 86400;
-
-// Variable to store the total file size
 $totalFileSize = 0;
 
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
     echo '<table class="table table-hover">';
-    echo '<thead><tr><th>Mod Name</th><th>File Size (MB)</th><th>Required?</th></tr></thead>';
+    echo '<thead><tr><th>Mod Name</th><th>Description</th><th>Image</th></tr></thead>';
     echo '<tbody>';
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         $link = $row['link'];
-        echo "<td>";
         $description = $row['description'];
-        echo "</td>";
-        echo "<td>";
         $image = $row['image'];
-        echo "</td>";
+        echo "<td>$link</td>";
+        echo "<td>$description</td>";
+        echo "<td><img src='$image' alt='$description' style='max-width: 100px;'></td>";
         echo "</tr>";
     }
     echo '</tbody>';
@@ -53,6 +48,7 @@ if ($result->num_rows > 0) {
 } else {
     echo "<p>No Mods found.</p>";
 }
+
 
 // Close the database connection
 $conn->close();
