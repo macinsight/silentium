6842ffb24c6bb4883ca8202d5d649cdde424b763
macinsight <hergemoeller@b1-systems.de>
2024-02-02 14:23:33 +0100

    Update bootstrap js script
    Signed-off-by: macinsight <hergemoeller@b1-systems.de>

--- index.php
+++ index.php
@@ -418,10 +418,7 @@
 							</ul>
 						</footer>
 					</div>
-
-					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-						integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-						crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
